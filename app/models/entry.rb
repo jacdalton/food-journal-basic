8305286810563023116cd6a60d29714b7ba62de7
Jacque Dalton <jacque.m.dalton@gmail.com>
2019-10-07 11:59:54 -0700

    Implemented simple calendar gem and refactored

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -3,4 +3,13 @@ class Entry < ApplicationRecord
   has_and_belongs_to_many :foods
 
   validates :entry_date, presence: true, uniqueness: { scope: :user }
+
+  def start_time
+    self.entry_date
+  end
+
+  def sum_calories
+    self.foods.map { |f| f.calories }.sum
+  end
+
 end
