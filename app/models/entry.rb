57c3cc411752ac3a1290c6b2eb03f5c3418a7768
Jacque Dalton <jacque.m.dalton@gmail.com>
2019-10-14 13:22:52 -0700

    Added validation and removed existing records from drop down

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,6 +1,10 @@
 class Entry < ApplicationRecord
   belongs_to :user
-  has_and_belongs_to_many :foods
+  has_and_belongs_to_many :foods, -> { distinct } do
+    def << (food)
+      super food rescue ActiveRecord::RecordNotUnique
+    end
+  end
 
   validates :entry_date, presence: true, uniqueness: { scope: :user }
 
