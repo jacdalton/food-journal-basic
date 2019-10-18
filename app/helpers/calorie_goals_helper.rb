1c181198b1f0e2c886c26def52ce271829ce78dd
Jacque Dalton <jacque.m.dalton@gmail.com>
2019-10-18 12:11:04 -0700

    Generated calorie goals controller with basic CRUD

---
+++ app/helpers/calorie_goals_helper.rb
@@ -0,0 +1,2 @@
+module CalorieGoalsHelper
+end
---
+++ test/controllers/calorie_goals_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class CalorieGoalsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
