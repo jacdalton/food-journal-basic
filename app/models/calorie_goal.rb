22784f73e19c7c65ebe067e7ffad2ef883eb6e2f
Jacque Dalton <jacque.m.dalton@gmail.com>
2019-10-21 15:57:27 -0700

    Calculate deficit/excess of calories based on historical goal

--- app/models/calorie_goal.rb
+++ app/models/calorie_goal.rb
@@ -2,4 +2,9 @@ class CalorieGoal < ApplicationRecord
   belongs_to :user
 
   validates :calories, presence: true
+
+  def self.max_calories(user, date)
+    q_date = DateTime.new(date.year, date.month, date.day, 23, 59, 59)
+    where('user_id = ? and created_at < ?', user, q_date).order(created_at: :desc).first
+  end
 end
