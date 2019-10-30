70dd1a2f47654ba602ef6ca0d02fd1b8810e722f
Jacque Dalton <jacque.m.dalton@gmail.com>
2019-10-30 11:29:23 -0700

    Integrated bmr calorie calculator

--- app/javascript/components/calculator.js
+++ app/javascript/components/calculator.js
@@ -9,10 +9,62 @@ const mifflinStJeorFemale = (weight, height, age) => {
   return (10 * weight) + (6.25 * height) - (5 * age) - 161
 }
 
-const convertImpToMet = (weight, feet, inches) => {
-  let results = {};
-  results['weight'] = weight / 0.453592;
-  results['height'] = ((feet * 12) + inches) * 2.54;
-
-  return results;
+const convertImpToMetWeight = (weight) => {
+  return weight * 0.453592;
 }
+
+const convertImpToMetHeight = (feet, inches) => {
+  return ((feet * 12) + inches) * 2.54;
+}
+
+const calcCalories = () => {
+  const inputs = getInputs();
+  const gender = document.querySelector('input[name=genderOptions]:checked').value;
+  const display = document.getElementById('calories');
+  const weightLoss = document.getElementById('one-pound');
+
+  if(gender) {
+    let bmr = 0;
+    if(gender === 'male'){
+      bmr = Math.round(mifflinStJeorMale(inputs.weight, inputs.height, inputs.age), 2);
+    } else if(gender === 'female') {
+      bmr = Math.round(mifflinStJeorFemale(inputs.weight, inputs.height, inputs.age), 2);
+    } else {
+      display.textContent = 'Please enter all info';
+    }
+    display.textContent = `BMR: ${bmr} calories/day`;
+    display.classList.add('btn');
+    weightLoss.textContent = `1-lb/week Weight Loss: ${bmr - 500} calories/day`;
+    weightLoss.classList.add('btn');
+  }
+}
+
+const getInputs = () => {
+  const age = document.getElementById('age').value;
+  const imperial = document.getElementById('nav-imperial').classList.contains('active');
+  let weight = 0;
+  let height = 0;
+
+  if(imperial) {
+    const impWeight = parseFloat(document.getElementById('imp-weight').value);
+    const feet = parseFloat(document.getElementById('feet').value);
+    const inches = parseFloat(document.getElementById('inches').value) || 0;
+    weight = convertImpToMetWeight(impWeight);
+    height = convertImpToMetHeight(feet, inches);
+  } else {
+    const metWeight = parseFloat(document.getElementById('met-weight').value);
+    const height = parseFloat(document.getElementById('met-height').value);
+    weight = metWeight;
+    height = height;
+  }
+
+  const inputs = {
+    age: age,
+    weight: weight,
+    height: height
+  }
+
+  return inputs;
+}
+
+export { calcCalories };
